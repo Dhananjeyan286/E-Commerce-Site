ef1350efe68a29d7c4039e6f4089cfa0d58c8424
dhananjeyan 286 <dhananjeyan28@gmail.com>
2021-09-08 13:59:49 +0530

    last try

--- backend/server.js
+++ backend/server.js
@@ -48,5 +48,5 @@ app.use(notfound)
 app.use(errorhandler)
 
 
-const port=process.env.port || 5000
-app.listen(port,"0.0.0.0",()=>{console.log(`Server running on ${process.env.node_env} mode on port ${port}`)})
+const PORT=process.env.PORT || 5000
+app.listen(PORT,"0.0.0.0",()=>{console.log(`Server running on ${process.env.node_env} mode on PORT ${PORT}`)})
